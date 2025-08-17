379d3451d67dcc500d1e847e5baedc0a4736dcc2
kristinka-skl <93260352+kristinka-skl@users.noreply.github.com>
2025-08-17 17:13:01 +0000

    Deploying to gh-pages from @ kristinka-skl/Blended-internet-store@e8a8fe687e4ed05906335a4ad6780773031a877f 🚀

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import"./assets/styles-JE8YjOlG.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";const s={listEl:document.querySelector(".categories"),productsListEl:document.querySelector(".products")},n=o.create({baseURL:"https://dummyjson.com/products"});async function c(){const t=await n.get("/category-list");return console.log(t),t.data}function a(t){return t.unshift("All"),t.map(r).join("")}function r(t){return`<li class="categories__item">
+import"./assets/styles-JE8YjOlG.js";import{a as i}from"./assets/vendor-N5iQpiFS.js";const c={listEl:document.querySelector(".categories"),productsListEl:document.querySelector(".products")},r=i.create({baseURL:"https://dummyjson.com/products"});async function u(){const t=await r.get("/category-list");return console.log(t),t.data}async function p(t=1){return(await r.get(`?limit=12&skip=${(t-1)*12}`)).data.products}function l(t){return t.unshift("All"),t.map(d).join("")}function d(t){return`<li class="categories__item">
    <button class="categories__btn" type="button">${t}</button>
  </li>
-`}async function i(){const t=await c(),e=a(t);s.listEl.innerHTML=e}i();
+`}function m({id:t,thumbnail:o,title:s,brand:e,category:a,price:n}){return`<li class="products__item" data-id="${t}">
+    <img class="products__image" src="${o}" alt="${s}"/>
+    <p class="products__title">${s}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${e}</span></p>
+    <p class="products__category">Category: ${a}</p>
+    <p class="products__price">Price:${n} $</p>
+ </li>
+`}function _(t){c.productsListEl.innerHTML=t.map(m).join("")}async function g(){const t=await u(),o=l(t);c.listEl.innerHTML=o;const s=await p();console.log(s),_(s)}g();
 //# sourceMappingURL=index.js.map
